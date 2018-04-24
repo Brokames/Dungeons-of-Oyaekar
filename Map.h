93c98f594772a471af7b36a768b58d1ac9e397e1
Brokames <32944238+Brokames@users.noreply.github.com>
2018-04-24 17:44:25 -0400

    Create Map.h

---
+++ Map.h
@@ -0,0 +1,26 @@
+#ifndef _MAP_H
+#define _MAP_H
+
+#include <iostream>
+#include <string>
+
+class Map {
+public:
+protected:
+private:
+};
+
+class Global: public Map{
+public:
+protected:
+private:
+};
+
+class Sub: public Map{
+public:
+protected:
+private:
+};
+
+
+#endif _MAP_H
