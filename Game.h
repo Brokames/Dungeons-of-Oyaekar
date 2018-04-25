c1daf9fc63c90380e6641c0f11f618ab58090e67
Brokames <32944238+Brokames@users.noreply.github.com>
2018-04-24 23:09:19 -0400

    Update Game.h

--- Game.h
+++ Game.h
@@ -1,11 +1,20 @@
 #ifndef _GAME_H
 #define _GAME_H
 
+#include "Entity.h"
+#include "Map.h"
+#include "Item.h"
+
+using namespace std;
 
 class Game {
 public:
+    Game();               //Creates a set of blank players and maps
+    void PlayNewGame();   //Places a blank new character and new map in chosen slot
+    void LoadGame();      //Loads into a already created character and map
 private:
+    Playable* p[3];
+    Map* m[3];
 };
-
 
 #endif //_GAME_H
