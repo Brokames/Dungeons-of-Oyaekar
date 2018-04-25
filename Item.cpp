fd8e956fc37fbc0bc034b89af68e079b325f33e1
Chaotic46 <38705677+Chaotic46@users.noreply.github.com>
2018-04-25 01:20:32 -0400

    Update Item.cpp

--- Item.cpp
+++ Item.cpp
@@ -1,6 +1,35 @@
 #include "Item.h"
 using namespace std;
 
-item::item(const string& s){
-  
+Item::Item(const string& s){
+  Name = s;
+}
+
+Consumable::Consumable(int H, int M, const string& s):Item(s){
+  HP = H;
+  MP = M
+}
+
+Wearable::Wearable(int H, int M, int S, int D, int I, const string& s):Item(s){
+  Defense = D;
+  Intel = I;
+  Strength = S;
+  HP = H;
+  MP = M;
+}
+
+OnHand::OnHand(int M, int Str, int Sp, int I, int A, const string& s):Item(s){
+  MP = M;
+  Strength = Str;
+  Speed = Sp;
+  Intel = I;
+  Accuracy = A;
+}
+
+OffHand::OffHand(int M, int Str, int D, int Sp, int I):Item(s){
+  MP = M;
+  Strength = Str;
+  Defense = D;
+  Speed = Sp;
+  Intel = I;
 }
