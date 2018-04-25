5acf6141d3382c5fe7bc4861dac92563240e55d9
Brokames <32944238+Brokames@users.noreply.github.com>
2018-04-25 14:08:25 -0400

    Update Item.cpp

--- Item.cpp
+++ Item.cpp
@@ -5,31 +5,46 @@ Item::Item(const string& s){
   Name = s;
 }
 
-Consumable::Consumable(int H, int M, const string& s):Item(s){
-  HP = H;
-  MP = M
-}
-
-Wearable::Wearable(int H, int M, int S, int D, int I, const string& s):Item(s){
-  Defense = D;
-  Intel = I;
-  Strength = S;
+//Initializes all member data
+Consumable::Consumable(int H, int M, const string& N):Item(N){
   HP = H;
   MP = M;
+  Strength = 0;
+  Defense = 0;
+  Speed = 0;
+  Intel = 0;
+  Accuracy = 0;
 }
 
-OnHand::OnHand(int M, int Str, int Sp, int I, int A, const string& s):Item(s){
+//Initializes all member data
+Wearable::Wearable(int H, int M, int Str, int Def, int Sp, int In, int Acc, const string& N):Item(N){
+  HP = H;
   MP = M;
   Strength = Str;
+  Defense = Def;
   Speed = Sp;
-  Intel = I;
-  Accuracy = A;
+  Intel = In;
+  Accuracy = Acc;
 }
 
-OffHand::OffHand(int M, int Str, int D, int Sp, int I):Item(s){
+//Initializes all member data
+OnHand::OnHand(int H, int M, int Str, int Def, int Sp, int In, int Acc, const string& N):Item(s){
+  HP = H;
   MP = M;
   Strength = Str;
-  Defense = D;
+  Defense = Def;
   Speed = Sp;
-  Intel = I;
+  Intel = In;
+  Accuracy = Acc;
+}
+
+//Initializes all member data
+OffHand::OffHand(int H, int M, int Str, int Def, int Sp, int In, int Acc, const string& N):Item(s){
+  HP = H;
+  MP = M;
+  Strength = Str;
+  Defense = Def;
+  Speed = Sp;
+  Intel = In;
+  Accuracy = Acc;
 }
